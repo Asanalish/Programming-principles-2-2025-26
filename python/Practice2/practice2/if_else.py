256bccb5465aea78503b1c2e5ef70e1b25c1e88f
Asanalish <asan4kik85@gmail.com>
2026-02-04 23:31:12 +0500

    Refactor if-else statements and add examples

--- python/Practice2/practice2/if_else.py
+++ python/Practice2/practice2/if_else.py
@@ -1,73 +1,93 @@
-# if_else_example
-a = 33
-b = 33
-if b > a:
-  print("b is greater than a")
-elif a == b:
-  print("a and b are equal")
+#If statements
+x = 5
+if x > 0:
+    print("Positive") # Check positive number
+  
+# Check password length
+password = "admin123"
+if len(password) >= 6:
+    print("Strong password")
 
-if a > b: print("a is greater than b")
+#Check even number
+num = 8
+if num % 2 == 0:
+    print("Even number")
 
-a = 2
-b = 330
-print("A") if a > b else print("B")
-
-a = 330
-b = 330
-print("A") if a > b else print("=") if a == b else print("B")
-
-a = 33
-b = 200
-
-if b > a:
-  pass  #avoid getting error
-
-
-
-
-# if_else_exercise
-a = 50
-b = 10
-if a>b:
-    print("Hello World")
-
-a = 50
-b = 10
-if a!=b:
-    print("Hello World")
-
-a = 50
-b = 10
-if a==b:
-    print("Yes")
+# If else statements
+score = 45
+if score >= 50:
+    print("Pass")
 else:
-    print("No")
+    print("Fail") # Pass or fail
 
-a = 50
-b = 10
-if a==b:
-    print("1")
-elif a > b:
-    print("2")
+# Number sign
+x = -3
+if x > 0:
+    print("Positive")
 else:
-    print("3")
+    print("Negative or zero")
 
-c=2
-d=2
-if a == b and c == d:
-  print("Hello")
+# Login check
+username = "admin"
+if username == "admin":
+    print("Welcome")
+else:
+    print("Access denied")
 
-if a == b or c == d:
-  print("Hello")
+# If elif else 
+score = 82
+if score >= 90:
+    print("A")
+elif score >= 75:
+    print("B")
+elif score >= 60:
+    print("C")
+else:
+    print("F")  #Grade system
 
-if 5 > 2:
- print("YES")
+# Temperature check
+temp = 18
+if temp > 30:
+    print("Hot")
+elif temp > 15:
+    print("Warm")
+else:
+    print("Cold")
 
-a = 2
-b = 50
-c = 2
-if a==c or b==c:
-    print("YES")
+# Traffic light
+light = "yellow"
+if light == "red":
+    print("Stop")
+elif light == "yellow":
+    print("Get ready")
+else:
+    print("Go")
+
+# Short hand if else (Ternary operator)
+num = 7
+result = "Even" if num % 2 == 0 else "Odd"
+print(result)
+
+a, b = 5, 9
+max_num = a if a > b else b
+print(max_num)
+
+is_logged = True
+print("Welcome") if is_logged else print("Please log in")
+
+
+# Switch if elif else 
+day = 3
+if day == 1:
+    print("Monday")
+elif day == 2:
+    print("Tuesday")
+elif day == 3:
+    print("Wednesday")
+else:
+    print("Unknown day") # Dat of the week
+  
+
 
 
 
